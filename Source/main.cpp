3baaf6548b3bcbe5f50a152d3397b6f4024ae35b
Jonatan <jonatanvkarlberg@gmail.com>
2019-08-07 13:02:08 +0300

    added testcode

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,34 @@
-#include <iostream
-
+#include <iostream>
+#include <curses.h>
+#include <wiringPi.h>
 int main(int argc, char* argv[])
 {
-
+	//Allocate Screen Memory
+	initscr();
+	
+	//Change Terminal Resolution
+	resizeterm(50, 50);
+	
+	
+	//------
+	//Remove all from screen
+	clear();
+	
+	//Make Changes
+	//TODO
+		
+	move(10,10);
+	printw("Hello World");
+	
+	//Render
+	refresh();
+	
+	
+	//TEMP GETCH
+	getch();
+	
+	//Free Memory
+	endwin();
 
 
 	return 0;
