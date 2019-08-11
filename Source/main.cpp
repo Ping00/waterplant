f3e3d4a5446c51ab29fc3b52f83ef28299983040
Jonatan <jonatan.karlberg@ha.ax>
2019-08-11 20:19:01 +0300

    fixed main loop to utilize engine

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,53 +1,13 @@
-#include <iostream>
-#include <curses.h>
-#include <wiringPi.h>
-#include <sstream>
-#include <string>
+#include "engine.hpp"
 int main(int argc, char* argv[])
 {
-	//Allocate Screen Memory
-	initscr();
-
-	//Change Terminal Resolution
-	resizeterm(50, 50);
-
-	int num = 0;
-
-	//------
-	//Make Changes
-	//TODO
-
-	while(true)
-    {
-        //HANDLE STEP
-
-        //Update step
-        num++;
-
-        std::stringstream ss;
-        ss << num;
-        std::string str = ss.str();
-
-        //RENDER STEP
-
-        //Clear screen from old clutter
-        move(10,10);
-        printw("Hello World");
-
-        clear();
-
-        //Render relevant parts
-
-        move(12,12);
-        printw(str.c_str());
-
-        //Render
-        refresh();
-
-    }
-	//Free Memory
-	endwin();
-
-
+	Engine engine;
+	engine.init();
+	while (engine.running())
+	{
+		engine.handle();
+		engine.update();
+		engine.render();
+	}
 	return 0;
 }
